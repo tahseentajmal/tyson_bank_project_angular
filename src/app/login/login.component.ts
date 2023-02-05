2d7f929ac4b995fe6e30b599cf2eb8d6c71556dc
Tahseen Tajmal <tahseentajmal99@gmail.com>
2023-02-05 12:06:29 +0530

    Second Commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,43 +7,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent {
 
-  data ="You perfect banking partner"
-  inputplaceholder ="Enter Account Number"
-  acno="" // or acno:any
-  password:any
+  data = "You perfect banking partner"
+  inputplaceholder = "Enter Account Number"
+  // acno = "" // or acno:any
+  // password: any
 
-  userdetails:any = {
-    1000:{acno:1000,username:"anu",password:"abc123",balance:10000},
-    1001:{acno:1001,username:"manu",password:"abc123",balance:20000},
-    1002:{acno:1002,username:"tanu",password:"abc123",balance:30000},
-    1003:{acno:1003,username:"fanu",password:"abc123",balance:40000},
-
-  }
-
-  constructor(){
+  userdetails: any = {
+    1000: { acno: 1000, username: "anu", password: "abc123", balance: 10000 },
+    1001: { acno: 1001, username: "manu", password: "abc123", balance: 20000 },
+    1002: { acno: 1002, username: "tanu", password: "abc123", balance: 30000 },
+    1003: { acno: 1003, username: "fanu", password: "abc123", balance: 40000 },
 
   }
 
-  ngOnInIt():void {
+  constructor() {
 
   }
 
-  login(){
-    alert("login success")
-  }
-  acnoChange(event:any){
+  ngOnInIt(): void {
 
-    this.acno=event.target.value
-    console.log(this.acno);
+  }
+
+  login(ac:any,pw:any) {  // Using Template rendering variable #variable in html
+    console.log(ac.value);
+    console.log(pw.value);
     
-  }
-  
-  passwordChange(event:any){
-
-    this.password=event.target.value
-    console.log(this.password);
     
+    var acnum = ac.value
+    var password = pw.value
+    var userdetails = this.userdetails
+    if (acnum in userdetails) {
+      if (password === userdetails[acnum]["password"]) {
+        alert("Login Success")
+      }
+      else {
+        alert("Incorrect Password")
+      }
+    }
+    else {
+      alert("Account number is incorrect")
+    }
   }
+
+  // login() {  using $ event
+  //   var acnum = this.acno
+  //   var password = this.password
+  //   var userdetails = this.userdetails
+  //   if (acnum in userdetails) {
+  //     if (password === userdetails[acnum]["password"]) {
+  //       alert("Login Success")
+  //     }
+  //     else {
+  //       alert("Incorrect Password")
+  //     }
+  //   }
+  //   else {
+  //     alert("Account number is incorrect")
+  //   }
+  // }
+
+
+  // acnoChange(event: any) {
+
+  //   this.acno = event.target.value
+  //   console.log(this.acno);
+
+  // }
+
+  // passwordChange(event: any) {
+
+  //   this.password = event.target.value
+  //   console.log(this.password);
+
+  // }
 
 }
 
